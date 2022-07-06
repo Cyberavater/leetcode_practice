248a9cc91a124539833bd7b3dfa42a931c0a7998
Cyberavater <32187927+Cyberavater@users.noreply.github.com>
2022-07-06 12:22:42 +0600

    Create 78-Subsets.kt

---
+++ 78-Subsets.kt
@@ -0,0 +1,32 @@
+fun main() {
+
+    val x = listOf(4, 1, 4, 4)
+    val v = Solution().subsetsWithDup(x.toIntArray())
+    println(v)
+}
+
+class Solution {
+    fun subsetsWithDup(nums: IntArray): List<List<Int>> {
+        val result = mutableListOf<List<Int>>()
+
+        nums.sort()
+
+        fun dfs(i: Int, list: MutableList<Int>) {
+            if (i == nums.size) {
+                result.add(list)
+            } else {
+                var k = 0
+                do {
+                    k++
+                } while (i + k < nums.size && nums[i + k] == nums[i])
+                dfs(i + k, list.toMutableList())
+                list.add(nums[i])
+                dfs(i + 1, list.toMutableList())
+            }
+        }
+
+        dfs(0, mutableListOf())
+
+        return result
+    }
+}
