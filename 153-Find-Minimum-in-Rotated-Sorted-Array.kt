812dcf86b8395cff5d33dd93c3314ebf37bd7489
Cyberavater <32187927+Cyberavater@users.noreply.github.com>
2022-07-12 02:33:27 +0600

    Create 153-Find-Minimum-in-Rotated-Sorted-Array.kt

---
+++ 153-Find-Minimum-in-Rotated-Sorted-Array.kt
@@ -0,0 +1,29 @@
+fun main() {
+
+    val x = listOf(11,13,15,17)
+    val v = Solution().findMin(x.toIntArray())
+    println(v)
+
+}
+
+class Solution {
+    fun findMin(nums: IntArray): Int {
+        if (nums.size==1){
+            return nums[0]
+        }
+        fun reFind(l: Int, r: Int): Int {
+            val midIndex = (l + r) / 2
+            val circularNext = (midIndex + 1) % nums.size
+            return if (nums[midIndex] > nums[circularNext]) {
+                nums[circularNext]
+            } else {
+                if (nums[l] > nums[midIndex]) {
+                    reFind(l, midIndex - 1)
+                } else {
+                    reFind(midIndex + 1, r)
+                }
+            }
+        }
+        return reFind(0, nums.lastIndex)
+    }
+}
