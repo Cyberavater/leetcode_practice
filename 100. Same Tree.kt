2fbcd64f1f02f8d53969bb5cbed2626d4ae24566
Cyberavater <32187927+Cyberavater@users.noreply.github.com>
2022-07-26 10:02:34 +0600

    Update 100. Same Tree.kt

--- 100. Same Tree.kt
+++ 100. Same Tree.kt
@@ -1,12 +1,9 @@
 class Solution {
     fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
         fun preOrderBi(node1: TreeNode?, node2: TreeNode?): Boolean {
-            if (node1 != null && node2 != null) {
-                if (node1.`val` != node2.`val`) {
-                    return false
-                }
-                return preOrderBi(node1.left, node2.left) && preOrderBi(node1.right, node2.right)
-            } else return node1 == null && node2 == null
+            return if (node1 != null && node2 != null) {
+                node1.`val` == node2.`val` && preOrderBi(node1.left, node2.left) && preOrderBi(node1.right, node2.right)
+            } else node1 == null && node2 == null
         }
         return preOrderBi(p, q)
     }
